0b4e42806b828f45816f55a4373085355614814a
Ackheron <ackheron@etik.com>
2025-10-29 16:40:01 +0100

    Mise en place de la liste des cocktails

--- src/app/components/cocktails/cocktails.ts
+++ src/app/components/cocktails/cocktails.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CocktailDetails } from './components/cocktail-details/cocktail-details';
+import { CocktailsList } from './components/cocktails-list/cocktails-list';
 
 @Component({
   selector: 'app-cocktails',
-  imports: [],
+  imports: [CocktailsList, CocktailDetails],
   templateUrl: './cocktails.html',
-  styleUrl: './cocktails.scss'
+  styleUrl: './cocktails.scss',
 })
-export class Cocktails {
-
-}
+export class Cocktails {}
