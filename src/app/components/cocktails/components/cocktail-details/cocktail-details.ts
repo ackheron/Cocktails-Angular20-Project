4b6ecfd95533cfdef351127e4c3912a225482375
Ackheron <ackheron@etik.com>
2026-05-25 15:37:29 +0200

    Changement description cocktail

--- src/app/components/cocktails/components/cocktail-details/cocktail-details.ts
+++ src/app/components/cocktails/components/cocktail-details/cocktail-details.ts
@@ -12,6 +12,6 @@ export class CocktailDetails {
     imageUrl:
       'https://static.750g.com/images/1200-675/dfe52623942a8b2e6b4f1e1715a42570/servez-et-degustez.jpg',
     name: 'Mojito',
-    description: `Le mojito est un cocktail classique cubain, idéal pour les moments de détente. Composé de rhum blanc, de feuilles de menthe fraîche, de jus de citron vert, de sucre, d'eau gazeuse et de glace pilée, il offre un équilibre parfait entre fraîcheur, acidité et douceur, avec des arômes rafraîchissants et tropicaux.`,
+    description: `Le mojito est un cocktail classique cubain, idéal pour les moments de détente. Composé de rhum blanc, de feuilles de menthe fraîche, de jus de citron vert, de sucre, d'eau gazeuse et de glace pilée, il offre un parfait équilibre entre fraîcheur, acidité et douceur, avec des arômes rafraîchissants et tropicaux.`,
   };
 }
